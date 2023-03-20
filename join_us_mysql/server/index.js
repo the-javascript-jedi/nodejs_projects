83a43cf10b4db5577bb51e798db932f5789be948
Nithin Samuel <nithin.thewebdev@gmail.com>
2023-03-20 22:24:06 +0530

    addded database connection to node

--- join_us_mysql/server/index.js
+++ join_us_mysql/server/index.js
@@ -3,7 +3,41 @@ const express = require("express");
 // const { searchTableWithPagination } = require("./controllers/pagination-table");
 // const { searchTableCount } = require("./controllers/pagination-table");
 const app = express();
+const mysql2 = require("mysql2");
 const { faker } = require("@faker-js/faker");
+
+// create connection
+const connection = mysql2.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "mysqlroot",
+  database: "db_join_us",
+});
+// test connection
+connection.connect((err) => {
+  if (err) throw err;
+  console.log("Connected!");
+});
+
+// my sql commands
+var q = "SELECT CURDATE()";
+// command 1
+connection.query("SELECT 1 + 1 AS solution", function (error, results, fields) {
+  if (error) throw error;
+  console.log("The solution is: ", results[0].solution);
+});
+// command 2
+var q = "SELECT CURTIME() as time, CURDATE() as date, NOW() as now";
+connection.query(q, function (error, results, fields) {
+  if (error) throw error;
+  // we can access the aliases created using as using dot notation in results
+  console.log(results[0].time);
+  console.log(results[0].date);
+  console.log(results[0].now);
+});
+// close connection
+connection.end();
+
 // SET Headers to overcome CROSS Origin requests
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
