59c8b7d2bd8f82ff915a047c56818e2ccfc04dd6
Nithin Samuel <nithin.thewebdev@gmail.com>
2023-11-16 22:11:26 +0530

    added server apis

---
+++ codevolution/angular-auth/readme.md
@@ -0,0 +1,7 @@
+# to start app
+
+i)run mongo db
+open powershell - mongod.exe
+
+ii)run node app
+npm run devStart
--- codevolution/angular-auth/server/routes/api.js
+++ codevolution/angular-auth/server/routes/api.js
@@ -36,4 +36,68 @@ router.post("/register", (req, res) => {
       console.log("err-- user.save()", err);
     });
 });
+
+router.post("/login", (req, res) => {
+  let userData = req.body;
+  User.findOne({ email: userData.email })
+    .then((user) => {
+      console.log("user", user);
+      if (!user) {
+        res.status(401).send("Invalid Email");
+      } else if (user.password !== userData.password) {
+        res.status(401).send("Invalid Password");
+      } else {
+        res.status(200).send(user);
+      }
+    })
+    .catch((err) => console.log(err));
+});
+
+router.get("/events", (req, res) => {
+  let events = [
+    {
+      _id: "1",
+      name: "Auto Expo 2012",
+      description: "lorem ipsum",
+      date: "2012",
+    },
+    {
+      _id: "2",
+      name: "Auto Expo 2013",
+      description: "lorem ipsum",
+      date: "2012",
+    },
+    {
+      _id: "3",
+      name: "Auto Expo 2014",
+      description: "lorem ipsum",
+      date: "2014",
+    },
+  ];
+  res.json(events);
+});
+
+router.get("/special", (req, res) => {
+  let events = [
+    {
+      _id: "1",
+      name: "Auto Expo 2012",
+      description: "lorem ipsum",
+      date: "2012",
+    },
+    {
+      _id: "2",
+      name: "Auto Expo 2013",
+      description: "lorem ipsum",
+      date: "2012",
+    },
+    {
+      _id: "3",
+      name: "Auto Expo 2014",
+      description: "lorem ipsum",
+      date: "2014",
+    },
+  ];
+  res.json(events);
+});
 module.exports = router;
