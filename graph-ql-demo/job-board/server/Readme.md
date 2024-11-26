62007947bc9445188983aa2e8d471e192d8801fd
Nithin Samuel <nithin.thewebdev@gmail.com>
2024-11-26 10:19:58 +0530

    jobs-board-initial commit

---
+++ graph-ql-demo/job-board/server/Readme.md
@@ -0,0 +1,2 @@
+node v18.20.2
+npm i @apollo/server graphql
---
+++ graph-ql-demo/job-board/server/server.js
@@ -0,0 +1,31 @@
+import cors from "cors";
+import express from "express";
+import { authMiddleware, handleLogin } from "./auth.js";
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware as apolloMiddleware } from "@apollo/server/express4";
+import { readFile } from "node:fs/promises";
+import { resolvers } from "./resolvers.js";
+
+const PORT = 9000;
+
+const app = express();
+app.use(cors(), express.json(), authMiddleware);
+
+app.post("/login", handleLogin);
+
+const typeDefs = await readFile("./schema.graphql", "utf8");
+
+const apolloServer = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+// start apollo server
+await apolloServer.start();
+// apply middleware to a path - And with this, Express will send all requests for
+// the "/graphql" path to the "apolloMiddleware",
+app.use("/graphql", apolloMiddleware(apolloServer));
+
+app.listen({ port: PORT }, () => {
+  console.log(`Server running on port ${PORT}`);
+  console.log(`GraphQL endpoint: http://localhost:${PORT}/graphql`);
+});
