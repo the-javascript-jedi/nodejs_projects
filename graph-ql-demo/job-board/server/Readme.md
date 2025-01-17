ff0facd74af0375f7c278992c35bf6f7a8f42b64
Nithin Samuel <nithin.thewebdev@gmail.com>
2025-01-17 19:24:29 +0530

    modified data for resolver

--- graph-ql-demo/job-board/server/Readme.md
+++ graph-ql-demo/job-board/server/Readme.md
@@ -1,2 +1,4 @@
 node v18.20.2
 npm i @apollo/server graphql
+
+O/P-http://localhost:9000/graphql
--- graph-ql-demo/job-board/server/resolvers.js
+++ graph-ql-demo/job-board/server/resolvers.js
@@ -1,12 +1,20 @@
 export const resolvers = {
   Query: {
-    job: () => {
-      return {
-        id: "test-id",
-        title: "The Title",
-        description: "The description",
-        test: "test Data",
-      };
+    jobs: () => {
+      return [
+        {
+          id: "test-id-1",
+          title: "The Title 1",
+          description: "The description 1",
+          testBatman: "test Data 1",
+        },
+        {
+          id: "test-id-2",
+          title: "The Title 2",
+          description: "The description 2",
+          testBatman: "test Data 2",
+        },
+      ];
     },
   },
 };
