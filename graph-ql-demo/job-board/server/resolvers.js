2ffbc49743f382815675a12762c5188e1b70c22c
Nithin Samuel <nithin.thewebdev@gmail.com>
2025-01-18 22:52:54 +0530

    added resolver and schema for company

--- graph-ql-demo/job-board/server/resolvers.js
+++ graph-ql-demo/job-board/server/resolvers.js
@@ -1,3 +1,4 @@
+import { getCompany } from "./db/companies.js";
 import { getJobs } from "./db/jobs.js";
 
 export const resolvers = {
@@ -10,6 +11,14 @@ export const resolvers = {
   },
 
   Job: {
+    company: (job) => {
+      // getCompany will return the id amd description of each company
+      // return {
+      //   id: "test-company-id",
+      //   description: "test-desc-id",
+      // };
+      return getCompany(job.companyId);
+    },
     date: (job) => {
       console.log("job", job);
       return job.createdAt.slice(0, "yyyy-mm-dd".length);
