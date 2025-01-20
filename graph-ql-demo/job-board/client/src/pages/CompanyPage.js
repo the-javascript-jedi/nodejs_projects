c94bd3ae124de929dbc1461b04146e6841c59723
Nithin Samuel <nithin.thewebdev@gmail.com>
2025-01-21 00:44:08 +0530

    added client and server

--- graph-ql-demo/job-board/client/src/pages/CompanyPage.js
+++ graph-ql-demo/job-board/client/src/pages/CompanyPage.js
@@ -5,15 +5,31 @@ import { getCompanies } from "../lib/graphql/queries";
 
 function CompanyPage() {
   const { companyId } = useParams();
-  const [company, setCompany] = useState();
+  const [state, setState] = useState({
+    company: null,
+    loading: true,
+    error: false,
+  });
 
   useEffect(() => {
-    getCompanies(companyId).then(setCompany);
+    (async () => {
+      try {
+        const company = await getCompanies(companyId);
+        setState({ company, loading: false, error: false });
+      } catch (error) {
+        console.log("error", JSON.stringify(error, null, 2));
+        setState({ company: null, loading: false, error: true });
+      }
+    })();
   }, [companyId]);
 
-  console.log("[CompanyPage] company:", company);
-  if (!company) {
+  console.log("[CompanyPage] state:", state);
+  const { company, loading, error } = state;
+  if (loading) {
     return <div>Loading...</div>;
+  }
+  if (error) {
+    return <div>Error Data Unavailable...</div>;
   }
   // const company = companies.find((company) => company.id === companyId);
   return (
