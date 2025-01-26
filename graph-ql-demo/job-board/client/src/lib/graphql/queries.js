78ecf0de87e3aa0fea6f1d7abd1a10ceeca956ec
Nithin Samuel <nithin.thewebdev@gmail.com>
2025-01-26 22:43:22 +0530

    added auth request for client

--- graph-ql-demo/job-board/client/src/lib/graphql/queries.js
+++ graph-ql-demo/job-board/client/src/lib/graphql/queries.js
@@ -1,6 +1,15 @@
 import { GraphQLClient, gql } from "graphql-request";
+import { getAccessToken } from "../auth";
 
-const client = new GraphQLClient("http://localhost:9000/graphql");
+const client = new GraphQLClient("http://localhost:9000/graphql", {
+  headers: () => {
+    const accessToken = getAccessToken();
+    if (accessToken) {
+      return { Authorization: `Bearer ${accessToken}` };
+    }
+    return {};
+  },
+});
 
 // mutation for post request - creating a job
 export async function createJobQuery({ title, description }) {
